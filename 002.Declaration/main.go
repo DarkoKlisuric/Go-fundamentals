1a606c9923a37c58a83e8ac2520803d788c99e0c
Darko Klisurić <klisuric1995@gmail.com>
2020-04-29 21:15:09 +0200

    iota

--- 002.Declaration/main.go
+++ 002.Declaration/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var b = 100
+
 // declare there is a variable with the identifier "c"
 // false for booleans, 0 for numeric types, "" for strings,
 // and nil for pointers, functions, interfaces, slices, channels, and maps.
@@ -11,7 +14,15 @@ var c int
 type Darko string
 
 var d Darko
-func main()  {
+
+const (
+	_  = iota
+	kb = 1 << (iota * 10)
+	mb = 1 << (iota * 10)
+	gb = 1 << (iota * 10)
+)
+
+func main() {
 	// DECLARE a variable and assign a value (of a certain type)
 	a := 100
 	fmt.Println(a)
@@ -25,8 +36,20 @@ func main()  {
 	foo()
 
 	fmt.Printf("%T\n", d) // main.Darko type
+
+	iotaDeclaration()
 }
 
-func foo()  {
+func foo() {
 	fmt.Println("Again;", b)
+}
+
+func iotaDeclaration() {
+	// kb := 1024
+	// mb := 1024 * kb
+	// gb := 1024 * mb
+
+	fmt.Printf("%d\t\t%b\n", kb, kb)
+	fmt.Printf("%d\t\t%b\n", mb, mb)
+	fmt.Printf("%d\t%b\n", gb, gb)
 }
