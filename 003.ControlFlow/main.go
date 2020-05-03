ca604bb983202d1c8d6d6f8ec91a38651df23b28
Darko Klisurić <klisuric1995@gmail.com>
2020-05-03 19:19:31 +0200

    More looping

--- 003.ControlFlow/main.go
+++ 003.ControlFlow/main.go
@@ -3,9 +3,35 @@ package main
 import "fmt"
 
 func main() {
+	// for loop
+
 	for i := 1; i <= 10; i++ {
 		for j := 1; j <= 10; j++ {
 			fmt.Printf("The outher loop: %d\t The inner loop %d\t Result %d\n", i, j, i*j)
 		}
 	}
+
+	// Standard while loop
+	x := 1
+	for x < 10 {
+		fmt.Println(x)
+		x++
+	}
+
+	fmt.Println("Done")
+
+	// Infinite loop - for(;;)
+	y := 5
+	for {
+		if y > 25 {
+			fmt.Println("Done")
+			break
+		}
+		y++
+	}
+	// Printig ASCII
+	for k := 33; k <= 122; k++ {
+		fmt.Printf("%v\t%#U\n", k, k)
+	}
+
 }
