72c8946849152b54a5acead96c8702b7885d660f
Darko Klisurić <klisuric1995@gmail.com>
2020-05-09 10:26:01 +0200

    Embedded sturct

--- 005.Struct/struct.go
+++ 005.Struct/struct.go
@@ -2,22 +2,36 @@ package main
 
 import "fmt"
 
-type Person struct {
+type person struct {
 	firstName string
-	lastName string
+	lastName  string
 }
+
+type programer struct {
+	person
+	working bool
+}
+
 func main() {
-	p1 := Person{
+	p1 := person{
 		firstName: "Darko",
-		lastName: "Klisurić",
-		}
-	p2 := Person{
+		lastName:  "Klisurić",
+	}
+	p2 := person{
 		firstName: "Marko",
 		lastName:  "Markić",
+	}
+
+	pro1 := programer{
+		person: person{
+			firstName: "Darko",
+			lastName: "Klisueic",
+		},
+		working: true,
 	}
 	fmt.Println(p1)
 	fmt.Println(p1.lastName)
 	fmt.Println(p2)
 	fmt.Println(p2.lastName)
-
+	fmt.Println(pro1, pro1.person.lastName, pro1.person.firstName, pro1.working)
 }
