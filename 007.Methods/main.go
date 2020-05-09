c5760e073c02e6b0bf20f53353d68e7e2f155e08
Darko Klisurić <klisuric1995@gmail.com>
2020-05-09 12:11:23 +0200

    Methods with resivers

---
+++ 007.Methods/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+type programer struct {
+	person
+	working bool
+}
+
+func (p programer) programming()  {
+	fmt.Println("Hello from method")
+}
+
+func main() {
+	p1 := programer{
+		person: person{
+			firstName: "Darko",
+			lastName: "Klisuric",
+		},
+		working: true,
+	}
+
+	fmt.Println(p1)
+	p1.programming()
+}
