4aa247ae6993b875fa02529fe884755310a3d60e
Darko Klisurić <klisuric1995@gmail.com>
2020-05-10 19:17:53 +0200

    Func returning func

---
+++ 011.ReturningFunc/returningFunc.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	s1 := foo()
+	fmt.Println(s1)
+
+	s2 := bar()
+
+	fmt.Println(s2()) // 451
+
+	fmt.Printf("%T\n", s2()) // int
+}
+
+func foo() string {
+	s := "Hello world"
+	return s
+}
+
+func bar() func() int {
+	return func() int {
+		return 451
+	}
+}
