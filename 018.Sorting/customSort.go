f944aac309ce7e71aafe590702ff7273858e7d07
Darko Klisurić <klisuric1995@gmail.com>
2020-05-12 19:53:12 +0200

    Sorting by name

--- 018.Sorting/customSort.go
+++ 018.Sorting/customSort.go
@@ -17,6 +17,8 @@ func (p Person) String() string {
 // ByAge implements sort.Interface for []Person based on the Age field
 type ByAge []Person
 
+type ByName []Person
+
 func (a ByAge) Len() int {
 	return len(a)
 }
@@ -29,6 +31,18 @@ func (a ByAge) Less(i, j int) bool {
 	return a[i].Age < a[j].Age
 }
 
+func (n ByName) Len() int {
+	return len(n)
+}
+
+func (n ByName) Swap(i, j int) {
+	n[i], n[j] = n[j], n[i]
+}
+
+func (n ByName) Less(i, j int) bool {
+	return n[i].FirstName < n[j].FirstName
+}
+
 func main() {
 	people := []Person{
 		{"Darko", 25},
@@ -36,8 +50,11 @@ func main() {
 		{"Ivan", 22},
 		{"Matija", 32},
 	}
-
 	fmt.Println(people)
+
 	sort.Sort(ByAge(people))
+	fmt.Println(people)
+
+	sort.Sort(ByName(people))
 	fmt.Println(people)
 }
