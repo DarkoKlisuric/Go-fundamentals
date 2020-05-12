fa1d926927007b4eb79c3cd6c3d311df0fe5ddbe
Darko Klisurić <klisuric1995@gmail.com>
2020-05-12 19:47:27 +0200

    Custom sort by age

---
+++ 018.Sorting/customSort.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	FirstName string
+	Age int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%s: %d", p.FirstName, p.Age)
+}
+
+// ByAge implements sort.Interface for []Person based on the Age field
+type ByAge []Person
+
+func (a ByAge) Len() int {
+	return len(a)
+}
+
+func (a ByAge) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a ByAge) Less(i, j int) bool {
+	return a[i].Age < a[j].Age
+}
+
+func main() {
+	people := []Person{
+		{"Darko", 25},
+		{"Marko", 30},
+		{"Ivan", 22},
+		{"Matija", 32},
+	}
+
+	fmt.Println(people)
+	sort.Sort(ByAge(people))
+	fmt.Println(people)
+}
